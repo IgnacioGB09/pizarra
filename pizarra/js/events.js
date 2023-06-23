bd5f5bfee767d39ca1bafdb6007e808184b72a35
Ignacio <125318844+IgnacioGB09@users.noreply.github.com>
2023-06-23 10:04:37 -0600

    Add files via upload

---
+++ pizarra/js/events.js
@@ -0,0 +1,182 @@
+window.onload = function () {
+    DIBUJAR.canvas = document.getElementById("micanva");
+    DIBUJAR.inicio();
+    DIBUJAR.lapiz = document.getElementById("Mylapiz");
+    DIBUJAR.borrador = document.getElementById("borrador");
+    DIBUJAR.btnlipiar = document.getElementById("limpiarPizarra");
+    DIBUJAR.imagen = document.getElementById("imagen")
+    DIBUJAR.btnGuardar = document.getElementById("guardar")
+
+    DIBUJAR.lapiz.onclick = function() {
+        
+        // dibujar
+        DIBUJAR.canvas.onmousedown = function(e) {
+        this.posicion = DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+        console.log(this.posicion);
+        this.dibujando = true;
+        }
+
+        DIBUJAR.canvas.ontouchstart = function (e) {
+            e.preventDefault();
+            this.posicion = DIBUJAR.ajustarPosicion(e.changedTouches[0].clientX - DIBUJAR.xreal, e.changedTouches[0].clientY - DIBUJAR.xreal);
+            console.log(this.posicion);
+            this.dibujando = true;
+          };
+
+
+
+        // no dibujar
+        DIBUJAR.canvas.onmouseup = function(e) {
+            DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+            this.dibujando = false;
+        }
+
+        /* DIBUJAR.canvas.ontouchend = function (e) {
+            e.preventDefault();
+            DIBUJAR.ajustarPosicion(
+              e.changedTouches[0].clientX,
+              e.changedTouches[0].clientY
+            );
+            this.dibujando = false;
+          } */
+       
+
+        // mantiene el dibujo
+        DIBUJAR.canvas.onmousemove  = function(e) {
+            if (this.dibujando) {
+            let fin = DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+            
+            // inicio y fin
+            DIBUJAR.dibujando(this.posicion, fin);
+
+            this.posicion = fin;
+            }
+        }
+
+        DIBUJAR.canvas.ontouchmove = function (e) {
+            e.preventDefault();
+            if (this.dibujando) {
+              let fin = DIBUJAR.ajustarPosicion(e.changedTouches[0].clientX - DIBUJAR.xreal, e.changedTouches[0].clientY - DIBUJAR.yreal);
+      
+              // inicio y fin
+              DIBUJAR.dibujando(this.posicion, fin);
+      
+              this.posicion = fin;
+            }
+        }
+        
+    }
+
+
+
+       
+
+
+      
+
+        
+
+   
+
+
+    DIBUJAR.borrador.onclick = function() {
+        
+        // borrar
+        DIBUJAR.canvas.onmousedown = function(e) {
+        this.posicion = DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+        console.log(this.posicion);
+        this.borrar = true;
+        }
+        DIBUJAR.canvas.ontouchstart = function (e) {
+            e.preventDefault();
+            this.posicion = DIBUJAR.ajustarPosicion(e.changedTouches.clientX, e.changedTouches[0].clientY);
+            console.log(this.posicion);
+            this.borrar = true;
+          }
+
+        // no borrarr
+        DIBUJAR.canvas.onmouseup = function(e) {
+            DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+            this.borrar = false;
+        }
+
+        
+
+        // mantiene el dibujo
+        DIBUJAR.canvas.onmousemove  = function(e) {
+            if (this.borrar) {
+            let fin = DIBUJAR.ajustarPosicion(e.clientX, e.clientY);
+            
+            // inicio y fin
+            DIBUJAR.borrar(this.posicion, fin);
+
+            this.posicion = fin;
+            }
+        }
+
+        DIBUJAR.canvas.ontouchmove = function (e) {
+            e.preventDefault();
+            if (this.borrar) {
+              let fin = DIBUJAR.ajustarPosicion(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+      
+              // inicio y fin
+              DIBUJAR.dibujando(this.posicion, fin);
+      
+              this.posicion = fin;
+            }
+        }
+    }
+    
+    DIBUJAR.btnlipiar.onclick = function () {
+        DIBUJAR.limpiar();
+    }
+
+    DIBUJAR.imagen.onclick = function () {
+        DIBUJAR.cargarImagen();
+    } 
+
+    DIBUJAR.btnGuardar.onclick = function () {
+        DIBUJAR.guardarImg();
+    }
+    
+
+    let red = document.getElementById("red"); //botones
+        let blue = document.getElementById("blue"); //botones
+        let green = document.getElementById("green"); //botones
+        let yellow = document.getElementById("yellow"); //botones
+        let black = document.getElementById("black"); //botones
+        let white = document.getElementById("white"); //botones 
+        white.addEventListener("click",() => {
+            color.value = "#ffffff";
+    
+        });
+
+        black.addEventListener("click",() => {
+            color.value = "#000000";
+    
+        });
+
+        blue.addEventListener("click",() => {
+            color.value = "#0000ff";
+    
+        });
+
+        red.addEventListener("click",() => {
+            color.value = "#ff0000";
+    
+        });
+
+        green.addEventListener("click",() => {
+            color.value = "#00ff00";
+    
+        });
+
+        yellow.addEventListener("click",() => {
+            color.value = "#ffff00";
+    
+        });
+    
+
+
+}
+
